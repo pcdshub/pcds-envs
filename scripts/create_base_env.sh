9b77909f3af018cf840c2341bc94981ec0e969f3
Zachary Lentz <zlentz@slac.stanford.edu>
2017-11-30 12:11:24 -0800

    ENH: Put our packages into the base definition

--- scripts/create_base_env.sh
+++ scripts/create_base_env.sh
@@ -9,14 +9,15 @@ fi
 conda create -y --name $ENVNAME \
   python=3.6 \
   ipython \
+  pydm \
+  pcds-devices=0.2.0 \
+  pswalker=0.2.0 \
+  lightpath=0.2.1 \
+  psbeam=0.0.3 \
   pydaq=current \
   pycdb=current \
   pyami=current \
-  pyqt=5.6 \
-  pyqtgraph \
   pyepics=3.2.7 \
-  bluesky \
-  ophyd \
   jupyter \
   opencv \
   xarray \
