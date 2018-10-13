c7c44070443e24223cd31a6fe57f691f6728af40
Zachary Lentz <zlentz@slac.stanford.edu>
2018-10-13 15:04:45 -0700

    MAINT: misc errors and inconsistencies

--- scripts/create_base_env.sh
+++ scripts/create_base_env.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Creates a "latest" environment from key packages
 if [ -z $1 ]; then
-  echo "Usage: create_base_env.sh [envname]"
+  echo "Usage: create_base_env.sh [envname] [python_ver]"
   exit
 else
   ENVNAME="${1}"
 fi
-if [ -z $2]; then
-  PY_VER='3.6'
+if [ -z $2 ]; then
+  PY_VER="3.6"
 else
   PY_VER="${2}"
 fi
