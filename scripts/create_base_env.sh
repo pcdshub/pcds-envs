40a21b37c1f3f0c2d73e04607e55be2887abaf1b
Zachary Lentz <zlentz@slac.stanford.edu>
2018-10-13 14:49:01 -0700

    CI: add matrixing for python 3.7

--- scripts/create_base_env.sh
+++ scripts/create_base_env.sh
@@ -6,7 +6,12 @@ if [ -z $1 ]; then
 else
   ENVNAME="${1}"
 fi
+if [ -z $2]; then
+  PY_VER='3.6'
+else
+  PY_VER="${2}"
+fi
 set -e
 source "$(dirname `which conda`)/../etc/profile.d/conda.sh"
-conda create -y --name $ENVNAME --file packages.txt
+conda create -y --name $ENVNAME python=$PY_VER --file packages.txt
 conda deactivate
