189facbb214aedbd0a8e60336cf1f53fd9f09143
Zachary Lentz <zlentz@slac.stanford.edu>
2019-09-06 15:59:42 -0700

    CI: bring out the tests into a standalone script and improve further

---
+++ run_all_tests.sh
@@ -0,0 +1,31 @@
+ERROR=0
+ERR_PKGS=""
+
+for dir in *
+do
+  if [ -f "${dir}/run_tests.py" ]; then
+    echo "Running tests for ${dir}"
+    pushd "${dir}"
+    # Check on the repo for debugging purposes
+    git status
+    git rev-parse HEAD
+    python run_tests.py
+    # Accumulate error codes, else the loop eats them
+    retcode=$?
+    if [ "${retcode}" -ne "0" ]; then
+      ERR_PKGS="${ERR_PKGS}\n${dir}"
+      color="31" # Red
+    else
+      color="32" # Green
+    fi
+    echo -e "\033[0;${color}mTest for ${dir} finished with exit code ${retcode}\033[0m"
+    (( ERROR += $retcode ))
+    popd
+  fi
+done
+
+# If ERROR is nonzero, at least one of the run_test.py calls failed
+if [ -n ${ERR_PKGS} ]; then
+  echo "The following packages had errors:${ERR_PKGS}"
+fi
+return $ERROR
