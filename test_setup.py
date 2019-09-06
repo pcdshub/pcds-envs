60700849423e41d6ea91cba4485211a6d16df005
Zachary Lentz <zlentz@slac.stanford.edu>
2019-09-06 11:52:26 -0700

    ENH: also check slaclab for tests, e.g. so we pick up pytmc tests

--- test_setup.py
+++ test_setup.py
@@ -29,20 +29,34 @@ def pushd(new_dir):
 
 
 if __name__ == '__main__':
+    print('Running pcds-envs test setup')
     args = parser.parse_args()
 
     info = version_info(['pcds-tag', 'pcds-dev'])
+    github_orgs = ['pcdshub', 'slaclab']
+    url_base = 'https://github.com/{org}/{pkg}'
+
+    if len(info) == 0:
+        print('No packages in current environment to test, quitting')
 
     for pkg, ver in info.items():
-        # Check if url exists
-        url = 'https://github.com/pcdshub/{}'.format(pkg)
-        resp = requests.head(url)
-        if resp.status_code >= 400:
-            print('Skip {}, no repo on pcdshub'.format(pkg))
+        print('Checking for tests on package {}...'.format(pkg))
+        for org in github_orgs:
+            # Check if url exists
+            url = url_base.format(org=org, pkg=pkg)
+            resp = requests.head(url)
+            if resp.status_code >= 400:
+                print('Skip {}/{}, nothing found'.format(org, pkg))
+                url = None
+            else:
+                print('Setting up test for {}/{}'.format(org, pkg))
+                break
+        if url is None:
             continue
         subprocess.run(['git', 'clone', url])
 
         if args.tag:
+            print('Checking out package tag')
             with pushd(pkg):
                 config = configparser.ConfigParser()
                 config.read('setup.cfg')
