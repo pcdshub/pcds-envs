320dd160ad056a8a699ce2a49be4eb0a29c0d4b1
Zachary Lentz <zlentz@slac.stanford.edu>
2020-01-07 09:53:43 -0800

    TST: fix syntax error and clean up a bit

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -48,15 +48,15 @@ fi
 
 # Do the environment's extra tests if they exist
 if [ -n "${1}" ]; then
-  EXTRA_TESTS=envs/${1}/extra_tests.sh
-  if [ -f "${EXTRA_TESTS}"]; then
+  EXTRA_TESTS="envs/${1}/extra_tests.sh"
+  if [ -f "${EXTRA_TESTS}" ]; then
     echo "Running ${EXTRA_TESTS}"
     bash "${EXTRA_TESTS}"
   else
     echo "Did not find ${EXTRA_TESTS}"
   fi
 else
-  echo "Did not input an environment name as argument 1"
+  echo "Skipping extra_tests.sh, did not input an environment name as argument 1"
 fi
 
 exit $ERROR
