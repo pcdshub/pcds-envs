0ad975e955e5f2ecaec230834d7f8f06be5bd39d
Zachary Lentz <zlentz@slac.stanford.edu>
2020-09-21 14:08:46 -0700

    CI: try to get CI to work for all builds again.

--- scripts/update_env.sh
+++ scripts/update_env.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 # Updates the previous environment to a new latest, rather than starting from scratch.
 if [ -z "${1}" ]; then
-  echo "Usage: update_env.sh [relnum] [base]"
+  echo "Usage: update_env.sh [envname] [base]"
   exit
 else
-  REL="${1}"
-fi
-if [ -z "${2}" ]; then
-  BASE="pcds"
-else
+  ENVNAME="${1}"
   BASE="${2}"
 fi
-ENVNAME="${BASE}-${REL}"
 set -e
 source "$(dirname `which conda`)/../etc/profile.d/conda.sh"
 ENV_DIR="../envs/${BASE}"
-conda env create --name "${ENVNAME}" --file "${ENV_DIR}/env.yaml"
+HASREL=`conda env list | grep "${NAME}"`
+if [ -z "${HASREL}" ]; then
+  conda env create --name "${ENVNAME}" --file "${ENV_DIR}/env.yaml"
+fi
 conda activate "${ENVNAME}"
 conda install --file "${ENV_DIR}/conda-packages.txt"
 pip install -r "${ENV_DIR}/pip-packages.txt"
