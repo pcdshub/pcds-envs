520d44f5e0f1aa08c4e642bdedc29bfc5673d3c4
Zachary Lentz <zlentz@slac.stanford.edu>
2020-09-21 09:55:16 -0700

    ENH: improve output for debug build.

--- scripts/conda_debug.sh
+++ scripts/conda_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run this script if the conda build fails and you don't know why.
-# This script creates a conda-debug environment, installing packages
+# This script creates a conda_debug environment, installing packages
 # one-by-one. In this way it becomes more apparent which packages are
 # incompatible, since the output from a failure to install 1 package
 # is more readable than the output for failing to install 50.
@@ -13,8 +13,10 @@ fi
 set -e
 source "$(dirname `which conda`)/../etc/profile.d/conda.sh"
 
-conda create -y --name conda-debug python=3.6
-conda activate conda-debug
+echo "Beginning debug build." | tee -a conda_debug.log
+conda info | tee -a conda_debug.log
+conda create -y --name conda_debug python=3.6 | tee -a conda_debug.log
+conda activate conda_debug
 
 PACKAGES="../envs/${ENV}/conda-packages.txt"
 cat "${PACKAGES}" | while read line
