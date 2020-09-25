c0412fa25b636f0b73aa9701e074ef33188a2c9b
Zachary Lentz <zlentz@slac.stanford.edu>
2020-09-25 09:58:58 -0700

    ENH: start with script from deployed env on NFS

---
+++ scripts/unpack.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Script to unpack a specific environment into a target directory
+ENV="${1}"
+DIR="${2}"
+
+if [ -z "${DIR}" ]; then
+  echo "Usage: unpack.sh <ENV> <DIR>"
+  echo "Example: unpack.sh pcds-3.3.4 /u1/conda"
+  echo "This will install /u1/conda/pcds-3.3.4"
+fi
+
+ENV_FILE="/reg/g/pcds/pyps/conda/packed_envs/${ENV}.tar.gz"
+TARGET="${DIR}/${ENV}"
+ACTIVATE="${TARGET}/bin/activate"
+DEACTIVATE="${TARGET}/bin/deactivate"
+
+if [ -f "${ENV_FILE}" ]; then
+  echo "Creating ${TARGET}"
+  mkdir "${TARGET}"
+  echo "Untarring ${ENV_FILE} into ${TARGET}"
+  tar -xzf "${ENV_FILE}" -C "${TARGET}"
+  if [ -f "${ACTIVATE}" ]; then
+    echo "Unpacking conda environment"
+    source "${ACTIVATE}"
+    conda-unpack
+    source "${DEACTIVATE}"
+    echo "Done!"
+  else
+    echo "Could not execute ${ACTIVATE}, something went wrong"
+    echo "Aborting"
+  fi
+else
+  echo "Could not find ${ENV_FILE}, something went wrong"
+  echo "Aborting"
+fi
