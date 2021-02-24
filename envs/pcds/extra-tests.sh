a99df8138ae9ef7492657d317bfaf243897c2e8c
Zachary Lentz <zlentz@slac.stanford.edu>
2021-02-23 16:28:59 -0800

    TST: add basic designer inclusion check

--- envs/pcds/extra-tests.sh
+++ envs/pcds/extra-tests.sh
@@ -1,7 +1,17 @@
 #! /usr/bin/env bash
 set -e
+
+# Check for basic entry point failures
 hutch-python --help
 lightpath --help
 pydm --help
 pytmc --help
 typhos --help
+
+# Check for inclusions in PYQTDESIGNERPATH
+for package in pydm typhos pcdswidgets; do
+  if [[ "${PYQTDESIGNERPATH}" != *"${package}"* ]]; then
+    echo "Did not find ${package} in PYQTDESIGNERPATH=${PYQTDESIGNERPATH}"
+    exit 1
+  fi
+done
