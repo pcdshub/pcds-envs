a2e9f2dcea41614b93440532a35ff989f6b307dc
Zachary Lentz <zlentz@slac.stanford.edu>
2023-04-04 17:28:17 -0700

    CI: experiment with staged builds

--- scripts/check_py_compat.sh
+++ scripts/check_py_compat.sh
@@ -12,12 +12,14 @@ if [ -z "${2}" ]; then
 else
   PY_VER="${2}"
 fi
+EXIT_CODE=0
 while read line;
 do
   if mamba create --dry-run --name debug_test python="${PY_VER}" "${line}" > /dev/null 2>&1; then
     echo "Found working package ${line}"
   else
     echo "Found broken package ${line}"
+    EXIT_CODE=1
   fi
 done < "../envs/${BASE}/conda-packages.txt"
-
+exit $EXIT_CODE
