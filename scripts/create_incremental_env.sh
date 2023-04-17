2b4df224fb73f3dc5df831e23b6977a73d4bc852
Zachary Lentz <zlentz@slac.stanford.edu>
2023-04-17 15:36:29 -0700

    WIP: one attempt at fixing the create incremental env script

--- scripts/create_incremental_env.sh
+++ scripts/create_incremental_env.sh
@@ -24,20 +24,28 @@ set -e
 
 ENVNAME="${BASE}-${REL}"
 ENV_DIR="../envs/${BASE}"
-source "$(dirname `which conda`)/../etc/profile.d/conda.sh"
+# shellcheck disable=SC1091
+source "$(dirname "$(which conda)")/../etc/profile.d/conda.sh"
 
-# Make a temp environment descriptor
-TEMP_ENV=".temp_env.yaml"
-git show "${GIT_REF}:envs/pcds/env.yaml" > "${TEMP_ENV}"
+# Make a temp environment descriptor without the pypi reqs
+TEMP_CONDA=".temp_env.yaml"
+git show "${GIT_REF}:envs/pcds/env.yaml" | grep -v "pip:" | grep -v "      -" > "${TEMP_CONDA}"
 
-# Create a copy of the old environment under the new name
-mamba env create -q -n "${ENVNAME}" -f "${TEMP_ENV}"
+# Create a copy of the old environment's conda specs under the new name
+mamba env create -q -n "${ENVNAME}" -f "${TEMP_CONDA}"
 
 # Update the copy minimally with our new specs
 mamba install -q -y -n "${ENVNAME}" --file "${ENV_DIR}/conda-packages.txt"
 conda activate "${ENVNAME}"
+
+# Invert the remove pips thing from earlier to get only the pip deps exactly"
+TEMP_PIP=".temp_pip.txt"
+git show "${GIT_REF}:envs/pcds/env.yaml" | grep "      -" | cut -c 9- > "${TEMP_PIP}"
+
+# Install exactly the versions we used last time from pip
+pip install -r "${TEMP_PIP}"
+# Install from the pinned latest versions in case something wants an update
 pip install -r "${ENV_DIR}/pip-packages.txt"
-${ENV_DIR}/extra-install-steps.sh
+"${ENV_DIR}"/extra-install-steps.sh
 ./install_activate.sh "${BASE}" "${ENVNAME}"
 conda deactivate
-
