191e06bc934727fc68bea1d49627b9391c635464
Zachary Lentz <zlentz@slac.stanford.edu>
2023-07-07 10:47:42 -0700

    ENH: include extras install as a build step + shellcheck

--- scripts/create_incremental_env.sh
+++ scripts/create_incremental_env.sh
@@ -42,8 +42,19 @@ git diff "${GIT_REF}" "${ENV_DIR}/conda-packages.txt" | grep "^+\w" | cut -c 2- 
 mamba install -q -y -n "${ENVNAME}" --file "${TEMP_CONDA_UP}"
 conda activate "${ENVNAME}"
 
+# First extras round to pick up conda stuff
+python get_extras.py "${BASE}" > "${ENV_DIR}"/extras_conda.txt
+mamba install --file "${ENV_DIR}"/extras_conda.txt
+
 # Install from the pinned latest versions in case something wants an update
-pip install -r "${ENV_DIR}/pip-packages.txt"
+pip install -r "${ENV_DIR}"/pip-packages.txt
+
+# Second extras round to pick up pypi stuff
+python get_extras.py "${BASE}" > "${ENV_DIR}"/extras_pip.txt
+# Looks redundant to force pypi to not "forget" about previous pins
+pip install -r "${ENV_DIR}"/pip-packages.txt -r "${ENV_DIR}"/extras_pip.txt
+
+# Environment can opt in to doing special steps at the end
 "${ENV_DIR}"/extra-install-steps.sh
 ./install_activate.sh "${BASE}" "${ENVNAME}"
 conda deactivate
