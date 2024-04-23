4aba97be81b9f41cba9527909cf0849cf1ea5483
Zachary Lentz <zlentz@slac.stanford.edu>
2024-04-23 16:30:14 -0700

    FIX: reinstate git actions in apply script

--- scripts/apply_release.sh
+++ scripts/apply_release.sh
@@ -8,8 +8,8 @@ fi
 set -e
 source "$(dirname `which conda`)/../etc/profile.d/conda.sh"
 echo "Updating to latest"
-#git checkout master
-#git pull origin master
+git checkout master
+git pull origin master
 if [ -z "${2}" ]; then
   BASE="pcds"
   TAG="${REL}"
@@ -22,8 +22,8 @@ YAML="${ENV_DIR}/env.yaml"
 NAME="${BASE}-${REL}"
 echo "Applying release ${NAME}"
 echo "Checking for tag ${TAG}"
-#git fetch origin
-#git checkout "${TAG}"
+git fetch origin
+git checkout "${TAG}"
 echo "Building environment"
 mamba env create -n "${NAME}" -f "${YAML}"
 ./install_activate.sh "${BASE}" "${NAME}"
@@ -34,5 +34,5 @@ echo "Write-protecting new env"
 pushd "${CONDA_ENVS_BASE}"
 chmod -R a-w ${NAME}
 popd
-#git checkout master
+git checkout master
 echo "Done"
